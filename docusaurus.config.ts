1d489932ad407518339c22d0f563e290952488cb
avciventures <mertcan@avci.ventures>
2026-06-06 10:26:40 +0300

    fix: move markdown warnings to hooks to fix TS and deprecation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,6 +23,14 @@ const config: Config = {
   projectName: 'documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  markdown: {
+    format: 'mdx',
+    mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+      onBrokenMarkdownImages: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
